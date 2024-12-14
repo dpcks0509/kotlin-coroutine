332b970d0281f3d089a668e0eb80f4943d301795
dpcks0509 <dpcks0509@gmail.com>
2024-12-15 01:25:00 +0900

    feat: 섹션3. CoroutineDispatcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     // 코루틴 라이브러리
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     // JUnit5 테스트 프레임워크
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.1")
