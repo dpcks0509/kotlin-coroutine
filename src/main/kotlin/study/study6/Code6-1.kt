cee5fbccda1d5b102e1eaa65df9c24d2411f3563
dpcks0509 <dpcks0509@gmail.com>
2024-12-15 16:54:36 +0900

    feat: 섹션6. CoroutineContext

---
+++ src/main/kotlin/study/study6/Code6-1.kt
@@ -0,0 +1,16 @@
+package study.study6
+
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+fun main() = runBlocking<Unit> {
+  val coroutineName = CoroutineName("MyCoroutine")
+  val dispatcher = Dispatchers.IO
+  val job = Job()
+  val coroutineContext: CoroutineContext = coroutineName + dispatcher + job
+
+  val deletedCoroutineContext = coroutineContext.minusKey(CoroutineName)
+
+  println("coroutineContext: $coroutineContext")
+  println("deletedCoroutineContext: $deletedCoroutineContext")
+}
